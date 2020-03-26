fa36ed814fd311279356c4fe80cc7db358e2938c
MkItSimple <choanania1@gmail.com>
2020-03-26 19:15:21 +0800

    Branch01 Completed

--- app/src/main/java/com/example/kotlinreyclerview2/CourseDetailActivity.kt
+++ app/src/main/java/com/example/kotlinreyclerview2/CourseDetailActivity.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.course_lesson_row.view.*
 import okhttp3.*
 import java.io.IOException
 
@@ -22,7 +23,7 @@ class CourseDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerView_main.layoutManager = LinearLayoutManager(this)
-        recyclerView_main.adapter = CourseDetailAdapter()
+//        recyclerView_main.adapter = CourseDetailAdapter()
 
         // we'll change the nav bar title..
         val navBarTitle = intent.getStringExtra(CustomViewHolder.VIDEO_TITLE_KEY)
@@ -46,7 +47,7 @@ class CourseDetailActivity : AppCompatActivity() {
 
 
                 runOnUiThread {
-                    recyclerView_main.adapter = CourseDetailAdapter()
+                    recyclerView_main.adapter = CourseDetailAdapter(courseLessons)
                 }
 
 //                println(body)
@@ -57,29 +58,34 @@ class CourseDetailActivity : AppCompatActivity() {
         })
     }
 
-    private class CourseDetailAdapter(): RecyclerView.Adapter<CourseLessonViewHolder>() {
+    private class CourseDetailAdapter(val courseLesson: Array<CourseLesson>): RecyclerView.Adapter<CourseLessonViewHolder>() {
+
+        override fun getItemCount(): Int {
+            return courseLesson.size
+        }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseLessonViewHolder {
-//            val blueView = View(parent.context)
-//            blueView.setBackgroundColor(Color.BLUE)
-//            blueView.minimumHeight = 50
-//            return CourseLessonViewHolder(blueView)
 
             val layoutInflater = LayoutInflater.from(parent.context)
             val customView = layoutInflater.inflate(R.layout.course_lesson_row, parent, false)
             return CourseLessonViewHolder(customView)
         }
 
-        override fun getItemCount(): Int {
-            return 5
-        }
-
         override fun onBindViewHolder(holder: CourseLessonViewHolder, position: Int) {
+            val courseLesson = courseLesson.get(position)
+
+            holder.customView.textView_course_lesson_title.text = courseLesson.name
+            holder.customView.textView_duration.text = courseLesson.duration
+
+            val imageView = holder.customView.imageView_course_lesson_thumbnail
+            Picasso.with(holder.customView.context).load(courseLesson.imageUrl).into(imageView)
+
+            holder.courseLesson = courseLesson
         }
 
     }
 
-    class CourseLessonViewHolder(val customView: View): RecyclerView.ViewHolder(customView) {
+    class CourseLessonViewHolder(val customView: View, var courseLesson: CourseLesson? = null): RecyclerView.ViewHolder(customView) {
 
         companion object {
             val COURSE_LESSON_LINK_KEY = "COURSE_LESSON_LINK"
@@ -89,6 +95,9 @@ class CourseDetailActivity : AppCompatActivity() {
             customView.setOnClickListener {
                 println("Attempt to load webview somehow???")
                 val intent = Intent(customView.context, CourseLessonActivity::class.java)
+
+                intent.putExtra(COURSE_LESSON_LINK_KEY, courseLesson?.link)
+
                 customView.context.startActivity(intent)
             }
         }
--- app/src/main/java/com/example/kotlinreyclerview2/CourseLessonActivity.kt
+++ app/src/main/java/com/example/kotlinreyclerview2/CourseLessonActivity.kt
@@ -12,15 +12,15 @@ class CourseLessonActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_course_lesson)
         webview_course_lesson.setBackgroundColor(Color.YELLOW)
-        webview_course_lesson.loadUrl("https://www.google.com")
 
-//        val courseLink = intent.getStringExtra(CourseDetailActivity.CourseLessonViewHolder.COURSE_LESSON_LINK_KEY)
+        val courseLink = intent.getStringExtra(CourseDetailActivity.CourseLessonViewHolder.COURSE_LESSON_LINK_KEY)
 
-//        webview_course_lesson.settings.javaScriptEnabled = true
-//        webview_course_lesson.settings.loadWithOverviewMode = true
-//        webview_course_lesson.settings.useWideViewPort = true
+        webview_course_lesson.settings.javaScriptEnabled = true
+        webview_course_lesson.settings.loadWithOverviewMode = true
+        webview_course_lesson.settings.useWideViewPort = true
 
-//        webview_course_lesson.loadUrl(courseLink)
+        //webview_course_lesson.loadUrl("https://www.google.com")
+        webview_course_lesson.loadUrl(courseLink)
     }
 
 }
